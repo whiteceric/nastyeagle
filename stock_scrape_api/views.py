9b5044c4a16800d84d058a85b4c1da639270df42
camwhite <whiteceric@gmail.com>
2021-06-07 08:54:35 -0400

    Added tickers.json for ticker lookups

--- stock_scrape_api/views.py
+++ stock_scrape_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import SymbolSerializer
 from .stock_scraper import get_current_price, get_prev_week_endpoints
+from datetime import datetime
 
 import json
 
@@ -13,7 +14,11 @@ import json
 def current_price(request, ticker):
     try:
         price = get_current_price(ticker)
-        return Response({'price': price, 'ticker': ticker})
+        return Response({
+            'price': price, 
+            'ticker': ticker,
+            'time-checked': datetime.utcnow().timestamp(),
+        })
     except: # add special except for the error we create in stock scraper
         return Response(status=status.HTTP_404_NOT_FOUND)
 
