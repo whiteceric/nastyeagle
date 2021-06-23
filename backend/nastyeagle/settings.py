e4608720c894319ebce3577271ae095872ec9418
camwhite <whiteceric@gmail.com>
2021-06-22 21:31:55 -0400

    Add heroku.yml

--- backend/nastyeagle/settings.py
+++ backend/nastyeagle/settings.py
@@ -80,17 +80,18 @@ WSGI_APPLICATION = 'nastyeagle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True, default='postgres://localhost')
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'postgres',
-#        'USER': 'postgres',
-#        'PASSWORD': 'postgres',
-#        'HOST': 'db',
-#        'PORT': '5432',
-#    }
-}
+DATABASES = dict()
+if os.environ.get('DATABASE_URL', False):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True) 
+else:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
 
 
 # Password validation
